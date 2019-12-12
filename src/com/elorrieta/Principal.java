6a64ed2ea446b6117f655116fbfa1973de0c12b0
Ana <Ana@gmail.com>
2019-12-12 16:53:59 +0100

    Añadir alumno 7

--- src/com/elorrieta/Principal.java
+++ src/com/elorrieta/Principal.java
@@ -1,14 +1,16 @@
 package com.elorrieta;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// Inicio del proyecto con Iker y Ana
-<<<<<<< HEAD
+
 // modifico a la vez que iker
-=======
+
 
 		alumno alumno1 = new alumno();
 		//alumno alumno1 = new alumno("Alain","8");
@@ -21,7 +23,11 @@ public class Principal {
 			alumno1.setNota(nota);
 			
 			System.out.println(alumno1.getNota());
->>>>>>> 81604348bccacf697fbecd49d15476e25c6d7ee0
+			
+			alumno alumno7 = new alumno("Asier","10");
+			
+			listAlumno.add(alumno7);
+
 	}
 
 }
